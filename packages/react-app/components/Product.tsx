fe2323fdee62b4761fcd4860b3a42f369e2965a4
Shun <shunbature02@gmail.com>
2023-07-01 22:47:12 +0100

    My feature request included in code functionalities

--- packages/react-app/components/Product.tsx
+++ packages/react-app/components/Product.tsx
@@ -1,4 +1,3 @@
-
 /* eslint-disable @next/next/no-img-element */
 // This component displays and enables the purchase of a product
 
@@ -36,8 +35,21 @@ const Product = ({ id, setError, setLoading, clear }: any) => {
   const { address } = useAccount();
   // Use the useContractCall hook to read the data of the product with the id passed in, from the marketplace contract
   const { data: rawProduct }: any = useContractCall("readProduct", [id], true);
+  //
+  const {data: productsCreated}: any = useContractCall("getProductCreated", [address], true)
+  //
+  const {data: deliciousReaction}: any = useContractCall("getReactions", [id, 1], true)
+  const {data: whateverReaction}: any = useContractCall("getReactions", [id, 2], true)
+  const {data: sourReaction}: any = useContractCall("getReactions", [id, 3], true)
+  const {data: allergicReaction}: any = useContractCall("getReactions", [id, 4], true)
+  const {data: expensiveReaction}: any = useContractCall("getReactions", [id, 5], true)
+  const {data: suspeciousReaction}: any = useContractCall("getReactions", [id, 6], true)
   // Use the useContractSend hook to purchase the product with the id passed in, via the marketplace contract
   const { writeAsync: purchase } = useContractSend("buyProduct", [Number(id)]);
+  //
+  const {writeAsync:setReaction} = useContractSend("setReaction", [Number(id), Number(0)]);
+
+  //
   const [product, setProduct] = useState<Product | null>(null);
   // Use the useContractApprove hook to approve the spending of the product's price, for the ERC20 cUSD contract
   const { writeAsync: approve } = useContractApprove(
@@ -53,9 +65,8 @@ const Product = ({ id, setError, setLoading, clear }: any) => {
       name: rawProduct[1],
       image: rawProduct[2],
       description: rawProduct[3],
-      location: rawProduct[4],
-      price: Number(rawProduct[5]),
-      sold: rawProduct[6].toString(),
+      price: Number(rawProduct[4]),
+      sold: rawProduct[5].toString(),
     });
   }, [rawProduct]);
 
@@ -63,7 +74,6 @@ const Product = ({ id, setError, setLoading, clear }: any) => {
   useEffect(() => {
     getFormatProduct();
   }, [getFormatProduct]);
-
 
   // Define the handlePurchase function which handles the purchase interaction with the smart contract
   const handlePurchase = async () => {
@@ -153,62 +163,67 @@ const Product = ({ id, setError, setLoading, clear }: any) => {
 
   // Return the JSX for the product component
   return (
-    <div className={"shadow-lg relative rounded-b-lg"}>
-      <p className="group">
-        <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden bg-white xl:aspect-w-7 xl:aspect-h-8 ">
-          {/* Show the number of products sold */}
-          <span
-            className={
-              "absolute z-10 right-0 mt-4 bg-amber-400 text-black p-1 rounded-l-lg px-4"
-            }
-          >
-            {product.sold} sold
-          </span>
-          {/* Show the product image */}
-          <img
-            src={product.image}
-            alt={"image"}
-            className="w-full h-80 rounded-t-md  object-cover object-center group-hover:opacity-75"
-          />
-          {/* Show the address of the product owner as an identicon and link to the address on the Celo Explorer */}
-          <Link
-            href={`https://explorer.celo.org/alfajores/address/${product.owner}`}
-            className={"absolute -mt-7 ml-6 h-16 w-16 rounded-full"}
-          >
-            {identiconTemplate(product.owner)}
-          </Link>
-        </div>
-
-        <div className={"m-5"}>
-          <div className={"pt-1"}>
-            {/* Show the product name */}
-            <p className="mt-4 text-2xl font-bold">{product.name}</p>
-            <div className={"h-40 overflow-y-hidden scrollbar-hide"}>
-              {/* Show the product description */}
-              <h3 className="mt-4 text-sm text-gray-700">
-                {product.description}
-              </h3>
-            </div>
+    <div className="relative group w-full lg:w-[370px]">
+      <div className="absolute -inset-1 bg-gradient-to-r from-purple-600 to-pink-600 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
+      <div className="relative px-1 py-1 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex flex-col justify-start">
+        <img className="h-[300px] rounded-t-md" src={product.image}/>
+        <div className="m-5">
+          <div className="mb-3 flex justify-between items-center m-auto">
+            <div className="text-3xl">{product.name}</div>
+            <div className="font-mono text-xl">${productPriceFromWei} cUSD</div>
           </div>
-
-          <div>
-            <div className={"flex flex-row"}>
-              {/* Show the product location */}
-              <img src={"/location.svg"} alt="Location" className={"w-6"} />
-              <h3 className="pt-1 text-sm text-gray-700">{product.location}</h3>
+          <div className="text-gray-500 my-4">{product.description}</div>
+          <div className="h-[60px] flex gap-2 justify-center items-center">
+            <div className="flex flex-col items-center">
+              <div className="text-gray-700">{deliciousReaction.length}</div>
+              <div className="text-[45px] hover:text-[60px] cursor-pointer" onClick={() => react("delicious", 1)}>&#128523;</div>
             </div>
-
-            {/* Buy button that calls the purchaseProduct function on click */}
-            <button
-              onClick={purchaseProduct}
-              className="mt-4 h-14 w-full border-[1px] border-gray-500 text-black p-2 rounded-lg hover:bg-black hover:text-white"
-            >
-              {/* Show the product price in cUSD */}
-              Buy for {productPriceFromWei} cUSD
+            <div className="flex flex-col items-center">
+              <div className="text-gray-700">{whateverReaction.length}</div>
+              <div className="text-[45px] hover:text-[60px] cursor-pointer" onClick={() => react("whatever", 2)}>&#128528;</div>
+            </div>
+            <div className="flex flex-col items-center">
+              <div className="text-gray-700">{sourReaction.length}</div>
+              <div className="text-[45px] hover:text-[60px] cursor-pointer" onClick={() => react("sour", 3)}>&#128534;</div>
+            </div>
+            <div className="flex flex-col items-center">
+              <div className="text-gray-700">{allergicReaction.length}</div>
+              <div className="text-[45px] hover:text-[60px] cursor-pointer" onClick={() => react("allergic", 4)}>&#129314;</div>
+            </div>
+            <div className="flex flex-col items-center">
+              <div className="text-gray-700">{expensiveReaction.length}</div>
+              <div className="text-[45px] hover:text-[60px] cursor-pointer" onClick={() => react("expensive", 5)}>&#129297;</div>
+            </div>
+            <div className="flex flex-col items-center">
+              <div className="text-gray-700">{suspeciousReaction.length }</div>
+              <div className="text-[45px] hover:text-[60px] cursor-pointer" onClick={() => react("suspecious", 6)}>&#129488;</div>
+            </div>
+            
+          </div>
+          <div className="flex justify-center items-center mt-8 mb-5 w-full">
+            <button onClick={purchaseProduct} className="w-full relative inline-flex items-center justify-center p-4 px-6 py-5 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-purple-500 rounded-full shadow-md group">
+            <span className="absolute z-10 right-4 mb-10 bg-green-400 text-black p-1 rounded-lg px-4">{product.sold} sold</span>
+              <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
+              <svg className="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 15a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0h8m-8 0-1-4m9 4a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-9-4h10l2-7H3m2 7L3 4m0 0-.792-3H1"/>
+              </svg>            
+              </span>
+              <span className="absolute flex items-center justify-center w-full h-full text-purple-500 transition-all duration-300 transform group-hover:translate-x-full ease">Buy</span>
+              <span className="relative invisible">Buy</span>
             </button>
           </div>
+          <hr className="w-full"/>
+          <div className="flex justify-start items-center gap-3 mt-4">
+            {identiconTemplate(product.owner)}
+            <div className="flex flex-col gap-1">
+              <Link href={`https://explorer.celo.org/alfajores/address/${product.owner}`}>
+                <div className="text-gray-500 font-mono text-sm w-[60px] overflow-hidden text-ellipsis">{product.owner}</div>
+              </Link>
+              <div className="text-gray-500">{Number(productsCreated)} products created</div>
+            </div>
+          </div>
         </div>
-      </p>
+      </div>
     </div>
   );
 };
